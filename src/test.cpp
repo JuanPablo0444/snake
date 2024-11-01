4a93735c235b9997b13d5ede382e653cf3e65be8
JuanPablo0444 <a23310374@ceti.mx>
2024-11-01 00:48:23 +0000

    clase 4

--- src/test.cpp
+++ src/test.cpp
@@ -12,45 +12,51 @@ int main(int argc, char const *argv[])
 {
     std::string reset_position;
     int frame = 0;
-
-
-   /* auto ColorSupport = vbox({
-
-                            Terminal::ColorSupport() >= Terminal::Color::Palette16
-                                ? text(" 16 color palette support : Yes")
-                                : text(" 16 color palette support : No"),
-                            Terminal::ColorSupport() >= Terminal::Color::Palette256
-                                ? text("256 color palette support : Yes")
-                                : text("256 color palette support : No"),
-                            Terminal::ColorSupport() >= Terminal::Color::TrueColor
-                                ? text("       True color support : Yes")
-                                : text("       True color support : No"),
-                        });*/
+    int conteo = 0;
     while (true)
     {
         auto can = Canvas(300,100);
-        can.DrawBlockLine(0,10,10,5);
-        can.DrawBlockLine(11,5,30,5);
-        can.DrawBlockLine(31,5,40,10);
-        
-        can.DrawBlockLine(80,10,90,5);
-        can.DrawBlockLine(91,5,110,5);
-        can.DrawBlockLine(111,5,120,10);
-
-        can.DrawBlockLine(0,60,30,70);
-        can.DrawBlockLine(30,70,90,70);
-        can.DrawBlockLine(90,70,120,60);
+        //Ceja izquierda
+        can.DrawBlockLine(50,10,60,5);
+        can.DrawBlockLine(60,5,80,5);
+        can.DrawBlockLine(81,5,90,10);
+        //Ceja derecha
+        can.DrawBlockLine(130,10,140,5);
+        can.DrawBlockLine(141,5,160,5);
+        can.DrawBlockLine(161,5,170,10);
         if(frame%4==0){
-            can.DrawBlockEllipse(20,20,20,1);
-            can.DrawBlockEllipse(100,20,20,1);
+            conteo++;//Cierra el ojo izquierdo
+            can.DrawBlockEllipse(70,20,20,1);
+            if(conteo%4==0){//Abre el ojo derecho
+                can.DrawPointCircle(150,20,8);
+                can.DrawBlockEllipse(150,20,20,8);
+                can.DrawPointCircle(150,20,3);
+                //Sonrisa
+                can.DrawBlockLine(50,60,170,60);
+                can.DrawBlockLine(50,60,80,75);
+                can.DrawBlockLine(80,75,140,75);
+                can.DrawBlockLine(140,75,170,60);
+                can.DrawPointLine(80,68,140,68);
+            }
+            else{//Cierra el ojo derecho
+                can.DrawBlockEllipse(150,20,20,1);
+                //Boca
+                can.DrawBlockLine(50,60,80,70);
+                can.DrawBlockLine(80,70,140,70);
+                can.DrawBlockLine(140,70,170,60); 
+            }
         }
-        else{
-            can.DrawPointCircle(20,20,8);
-            can.DrawBlockEllipse(20,20,20,8);
-            can.DrawPointCircle(20,20,3);
-            can.DrawPointCircle(100,20,8);
-            can.DrawBlockEllipse(100,20,20,8);
-            can.DrawPointCircle(100,20,3);
+        else{//Ojos
+            can.DrawPointCircle(70,20,8);
+            can.DrawBlockEllipse(70,20,20,8);
+            can.DrawPointCircle(70,20,3);
+            can.DrawPointCircle(150,20,8);
+            can.DrawBlockEllipse(150,20,20,8);
+            can.DrawPointCircle(150,20,3);
+            //Boca
+            can.DrawBlockLine(50,60,80,70);
+            can.DrawBlockLine(80,70,140,70);
+            can.DrawBlockLine(140,70,170,60);
         }
         //Crear pantalla y agregar canvas
         Screen pantalla = Screen::Create(Dimension::Full(), Dimension::Full());
@@ -61,7 +67,7 @@ int main(int argc, char const *argv[])
         pantalla.Print();
         reset_position = pantalla.ResetPosition(true);
         //Detener el programa 
-        this_thread::sleep_for(0.2s);
+        this_thread::sleep_for(0.3s);
         frame++;
     }
 
